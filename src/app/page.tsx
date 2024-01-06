641db2614841f062e02ff54f4c12004fb391146d
syedddanishalinaqvi <69369581+syedddanishalinaqvi@users.noreply.github.com>
2024-01-06 22:19:44 +0530

    frontend complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Post from "@/components/Post";
+import PostItem from "@/components/PostItem";
 
 export default function Home() {
   return (
@@ -9,7 +10,7 @@ export default function Home() {
       <div className="page-content">
         <div className="posts">
           {
-
+            <PostItem/>
           }
         </div>
       </div>
--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -1,20 +1,66 @@
 import React from "react";
+import "../css/PostItem.css";
+import Image from "next/image";
+import dp from "../assets/dp.jpeg";
+import { FaRegHeart } from "react-icons/fa";
+import { FaRegComment } from "react-icons/fa";
+import { FaRegBookmark } from "react-icons/fa";
+import Link from "next/link";
+
+const items = [
+  {
+    name: "Like",
+    icon: FaRegHeart,
+    onclick: "",
+  },
+  {
+    name: "Comment",
+    icon: FaRegComment,
+    onclick: "",
+  },
+  {
+    name: "Bookmark",
+    icon: FaRegBookmark,
+    onclick: "",
+  },
+];
 
 const PostItem = () => {
   return (
-    <div className="postitem">
-      <div className="postitem-content">
-        <div className="postitem-dp"></div>
-        <div className="postitem-data">
-          <div className="name-account">
-            <p>Name</p>
-            <p>@account</p>
+    <Link href="/">
+      <div className="postitem">
+        <div className="postitem-contents">
+          <div className="postitem-content">
+            <div className="postitem-dp">
+              <Image src={dp} height={40} width={40} alt="dp"></Image>
+            </div>
+            <div className="postitem-data">
+              <div className="name-account">
+                <p>
+                  <b>Name</b>
+                </p>
+                <p>@account</p>
+              </div>
+              <div className="data">
+                <p>
+                  I think this app looks amazing i should start working more on
+                  this app. Still it looks like it willl not work
+                </p>
+              </div>
+            </div>
           </div>
-          <div className="data"></div>
-          <div className="postitem-buttons"></div>
+          <div className="postitem-buttons">
+              {items.map((element) => {
+                return (
+                  <div key={element.name} title={element.name}>
+                    <element.icon />
+                  </div>
+                );
+              })}
+            </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
